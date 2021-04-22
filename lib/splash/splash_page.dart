7d61e11234f93c9932d44a231975eb6cbe48649b
paulo.roberto <paulo.roberto@seduc.ce.gov.br>
2021-04-22 18:58:09 -0300

    Splash page Gradiente e Image

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,8 +1,16 @@
+import 'package:DevQuiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(gradient: AppGradients.linear),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
   }
 }
